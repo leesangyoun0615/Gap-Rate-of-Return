7901d432b646e6d2845894a926311f30ec688e5c
HO_NAL_DO_7 <142569173+leesangyoun0615@users.noreply.github.com>
2024-05-09 08:05:13 +0900

    Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,21 +1,53 @@
 import logo from "../../assets/calcul.png";
 import "../Header/Header.css";
 
-export default function Header() {
+export default function Header({ selectedTopic }) {
+  let Description;
+
+  if (selectedTopic === "PBR") {
+    Description = (
+      <span className="text-container">
+        <p id="description">
+          Expected rate of return is the 'discount rate' and 'the gap between
+          the value applied at that discount rate and the current price' <br />
+          <span className="targetValue">
+            ex. DiscountRate = 10% , ROE = 15% <br /> Expected sustainability
+            period = 7years , <br />
+            appropriate P/B ratio =1.37
+            <br />{" "}
+          </span>
+          <span className="theReslut">
+            Gap Rate of Return = appropriate P/B ratio - market's P/B ratio
+          </span>
+        </p>
+      </span>
+    );
+  } else if (selectedTopic === "Annual Yield") {
+    Description = (
+      <span className="text-container">
+        <p id="description">
+          Expected rate of return is the 'discount rate' and 'the gap between
+          the value applied at that discount rate and the current price' <br />
+          <span className="targetValue">
+            ex. PBR = 1.05, ROE = 15% <br /> Expected sustainability period =
+            7years , <br />
+            AnnualYield = 14.2%
+            <br />{" "}
+          </span>
+          <span className="theReslut">
+            By inverting PBR, you can find the average annual rate of return.
+          </span>
+        </p>
+      </span>
+    );
+  }
+
   return (
     <header id="header">
       <h1>Gap Rate of Return</h1>
       <div className="image-container">
         <img src={logo} alt="logic" />
-        <span className="text-container">
-          <p>
-            위 코드에서 left: 50%;와 transform: translateX(-50%);를 사용하여
-            컨테이너를 가운데로 정렬하고 있습니다. max-width를 사용하여
-            컨테이너의 최대 너비를 이미지의 너비에서 패딩값을 뺀 값으로 설정하여
-            좌우 균형을 맞춥니다. 이렇게 하면 이미지와 컨테이너가 좌우로
-            균형있게 맞춰질 것입니다.
-          </p>
-        </span>
+        {Description}
       </div>
     </header>
   );
